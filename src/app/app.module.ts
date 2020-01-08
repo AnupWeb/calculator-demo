3c0552b9a3ab895c6056ccaa85ab89f1660fc401
anup.singh@luezoid.com <anup.singh@luezoid.com>
2020-01-08 11:36:46 +0530

    create calculator-screen component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculatorScreenComponent } from './calculator-screen/calculator-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorScreenComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/calculator-screen/calculator-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-screen',
+  templateUrl: './calculator-screen.component.html',
+  styleUrls: ['./calculator-screen.component.scss']
+})
+export class CalculatorScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
